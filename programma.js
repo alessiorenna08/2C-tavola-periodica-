6517624fa07c21b2312b5d316b485da3236bc23c
alessiorenna08 <alessio.renna@iismontalegenova.it>
2024-11-21 10:31:04 +0100

    scheda elemento

--- programma.js
+++ programma.js
@@ -2,7 +2,6 @@
 
 for (const element of elementi) {
     let contenitore = document.getElementById('contenitore')
-    contenitore.style.borderStyle = 'solid'; 
     contenitore.style.position = 'relative'; 
     
     let elemento = document.createElement('div')
@@ -14,6 +13,28 @@ for (const element of elementi) {
     elemento.style.left = 100 * (element.xpos - 1) + 'px';
     elemento.style.top = 120 * (element.ypos - 1) + 'px';
     elemento.style.backgroundColor ='#' + element ['cpk-hex']; 
+    elemento.style.boxSizing = 'border-box';
+    elemento.style.borderWidth = '0.5px';
+
+    elemento.addEventListener('click', () => {
+        let immagineScheda = document.getElementById('immagine-scheda');
+        immagineScheda.src = element.image.url; 
+        immagineScheda.style.borderStyle = 'solid';
+        immagineScheda.style.borderColor = '#' + element ['cpk-hex'];
+        immagineScheda.style.borderWidth = '8px'
+
+        let titoloScheda = document.getElementById('titolo-scheda');
+        titoloScheda.innerText = element.name;
+
+        let descrizioneScheda = document.getElementById('descrizione-scheda');
+        descrizioneScheda.innerText = element.summary; 
+
+        let categoriaScheda = document.getElementById('categoria-scheda');
+        categoriaScheda.innerText = 'category: ' + element.category; 
+
+        let densitàScheda = document.getElementById('densità-scheda'); 
+        densitàScheda.innerText = 'density: ' + element.density; 
+    })
     
     
     let numeroAtomico = document. createElement('p')
@@ -24,7 +45,7 @@ for (const element of elementi) {
     let simbolo = document.createElement('a')
     simbolo.innerText = element.symbol; 
     simbolo.style.textDecoration = 'none';
-    simbolo.style
+    simbolo.style.fontWeight = 'bold'
 
      
 
